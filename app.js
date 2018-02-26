5d43e6bc8e62c269d8c25cba72efa2421220901b
=natefikru <natefikru@gmail.com>
2018-02-25 19:40:24 -0600

    added static files and gcp configuration

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 var SwaggerExpress = require('swagger-express-mw')
 var express = require('express')
 var app = express()
+var path = require('path')
 
 var swaggerConfig = {
   appRoot: __dirname // required config
 }
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/api/swagger/index.html'))
+// })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 SwaggerExpress.create(swaggerConfig, function (err, swaggerExpress) {
   if (err) { throw err }
--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   url: 'mongodb://localhost:27017/',
-  dbName: 'wheniwork_db'
+  dbName: 'schedule-api-db'
 }
